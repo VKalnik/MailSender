dbc32907f94d2a1eca99b3a12d686ae07a201837
Victor Kalnik <v.kalnik@mail.ru>
2021-06-16 04:08:43 +0300

    Переименовано MainWindow в WpfMailSender в основном приложении. Оставил Title="Рассыльщик почты"

--- MailSender/WpfMailSender.xaml.cs
+++ MailSender/WpfMailSender.xaml.cs
@@ -18,9 +18,9 @@ namespace MailSender
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class WpfMailSender : Window
     {
-        public MainWindow()
+        public WpfMailSender()
         {
             InitializeComponent();
         }
