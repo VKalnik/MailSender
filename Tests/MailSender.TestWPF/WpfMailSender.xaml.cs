b28ec0dcf661364e1685288c9514bc15b6f35a25
Victor Kalnik <v.kalnik@mail.ru>
2021-06-21 16:31:08 +0300

    Игра с привязками

--- Tests/MailSender.TestWPF/WpfMailSender.xaml.cs
+++ Tests/MailSender.TestWPF/WpfMailSender.xaml.cs
@@ -7,18 +7,18 @@ namespace MailSender.TestWPF
     {
         public WpfMailSender() => InitializeComponent();
 
-        private void SendButton_OnClick(object sender, RoutedEventArgs e)
-        {
+        //private void SendButton_OnClick(object sender, RoutedEventArgs e)
+        //{
 
-            using (var mailSenderServiceClass = new MailSenderServiceClass(LoginEdit.Text, PasswordEdit.SecurePassword, MailSenderTestWpfConfig.SmtpServer, MailSenderTestWpfConfig.SmtpServerPort, MailSenderTestWpfConfig.SendersAddress))
-            {
-                mailSenderServiceClass.Send(SubjectEdit.Text + " Время отправки: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff"), BodyEdit.Text, MailSenderTestWpfConfig.RecipientAddress);
-            };
-        }
+        //    using (var mailSenderServiceClass = new MailSenderServiceClass(LoginEdit.Text, PasswordEdit.SecurePassword, MailSenderTestWpfConfig.SmtpServer, MailSenderTestWpfConfig.SmtpServerPort, MailSenderTestWpfConfig.SendersAddress))
+        //    {
+        //        mailSenderServiceClass.Send(SubjectEdit.Text + " Время отправки: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff"), BodyEdit.Text, MailSenderTestWpfConfig.RecipientAddress);
+        //    };
+        //}
 
-        private void Exit_OnClick(object sender, RoutedEventArgs e)
-        {
-            Close();
-        }
+        //private void Exit_OnClick(object sender, RoutedEventArgs e)
+        //{
+        //    Close();
+        //}
     }
 }
