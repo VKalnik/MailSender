cdf982e3e73f7fbb5b5ecfedce3130c42bc079ad
Victor Kalnik <v.kalnik@mail.ru>
2021-06-22 17:50:33 +0300

    Модель-представления

--- Tests/MailSender.TestWPF/ViewModels/MainWindowViewModel.cs
+++ Tests/MailSender.TestWPF/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,21 @@
-﻿namespace MailSender.TestWPF.ViewModels
-{
-    public class MainWindowViewModel
-    {
+﻿using System.ComponentModel;
 
+namespace MailSender.TestWPF.ViewModels
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _Title = "Hello World!";
+
+        public string Title
+        {
+            get => _Title;
+            set
+            {
+                _Title = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Title"));
+            }
+        }
     }
 }
