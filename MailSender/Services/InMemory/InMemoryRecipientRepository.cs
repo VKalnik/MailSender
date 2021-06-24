78e72fe92be3224aaa20293cc53cfe3833d95252
Victor Kalnik <v.kalnik@mail.ru>
2021-06-24 17:59:29 +0300

    Репозиторий отправителей/получателей

---
+++ MailSender/Services/InMemory/InMemoryRecipientRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MailSender.Models;
+
+namespace MailSender.Services.InMemory
+{
+    public class InMemoryRecipientRepository : InMemoryPersonsRepository<Recipient>
+    {
+        private static IEnumerable<Recipient> GetTestData(int Count = 10) => Enumerable.Range(1, Count)
+           .Select(
+                i => new Recipient
+                {
+                    Name = $"Получатель {i}",
+                    Address = $"recipient-{i}.server.ru",
+                    Description = $"Описание получателя {i}"
+                });
+
+        public InMemoryRecipientRepository() : base(GetTestData()) { }
+    }
+}
