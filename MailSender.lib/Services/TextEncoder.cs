35886100059b0511c16468a2f406f9e066d9a863
Victor Kalnik <v.kalnik@mail.ru>
2021-06-28 02:11:18 +0300

    Класс кодирования строк "Шифром Цезаря"

---
+++ MailSender.lib/Services/TextEncoder.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+namespace MailSender.Services
+{
+    public static class TextEncoder
+    {
+        public static string Encode(string srt, int Key = 1)
+        {
+            return new (srt.Select(c=>(char) (c + Key)).ToArray());
+        }
+
+        public static string Decode(string srt, int Key = 1)
+        {
+            return new(srt.Select(c => (char)(c - Key)).ToArray());
+        }
+    }
+}
