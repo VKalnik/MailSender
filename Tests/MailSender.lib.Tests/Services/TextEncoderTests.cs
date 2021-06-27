b81fe8b40418ce125fa8cde078ea7c33042a1f1b
Victor Kalnik <v.kalnik@mail.ru>
2021-06-28 02:53:19 +0300

    Модульные тесты для кодировщика строк

---
+++ Tests/MailSender.lib.Tests/Services/TextEncoderTests.cs
@@ -0,0 +1,49 @@
+﻿using MailSender.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MailSender.lib.Tests.Services
+{
+    [TestClass]
+    public class TextEncoderTests
+    {
+        [TestMethod]
+        public void Encode_ABC_return_BCD_with_Key_1()
+        {
+            // A-A-A
+
+            //Arrange - подготовка данных
+            var str = "ABC";
+            var Key = 1;
+            var exepted_result = "BCD";
+            
+            //Act - действие, нацеленное на тестирование кода
+            var actual_result = TextEncoder.Encode(str, Key);
+
+            //Assert - проверка утверждений
+            Assert.AreEqual(exepted_result, actual_result);
+            
+            //StringAssert.Matches();
+            //CollectionAssert.AreEquivalent();
+        }
+
+        [TestMethod]
+        public void Decode_BCD_return_ABC_with_Key_1()
+        {
+            // A-A-A
+
+            //Arrange - подготовка данных
+            var str = "BCD";
+            var Key = 1;
+            var exepted_result = "ABC";
+
+            //Act - действие, нацеленное на тестирование кода
+            var actual_result = TextEncoder.Decode(str, Key);
+
+            //Assert - проверка утверждений
+            Assert.AreEqual(exepted_result, actual_result);
+
+            //StringAssert.Matches();
+            //CollectionAssert.AreEquivalent();
+        }
+    }
+}
