d1bf8eeca537625253831d939436524a8a587755
Victor Kalnik <v.kalnik@mail.ru>
2021-06-30 14:56:50 +0300

    Очистили консольное приложение

--- Tests/MailSender.TestConsole/Program.cs
+++ Tests/MailSender.TestConsole/Program.cs
@@ -1,25 +1,10 @@
-﻿using System;
-using System.Net;
-using System.Net.Mail;
-
-namespace MailSender.TestConsole
+﻿namespace MailSender.TestConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            using var message = new MailMessage("kalnik.victor@yandex.ru", "kalnik.victor@yandex.ru");
-
-            using var client = new SmtpClient("smtp.yandex.ru", 25);
-
-            client.EnableSsl = true;
-            client.Credentials = new NetworkCredential
-            {
-                UserName = "kalnik.victor",
-                Password = "123"
-            };
-
-            client.Send(message);
+            
         }
     }
 }
