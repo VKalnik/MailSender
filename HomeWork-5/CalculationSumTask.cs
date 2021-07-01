7cd6b5e95e044d0a2f3f2a0bd5df751faa9c7cee
Victor Kalnik <v.kalnik@mail.ru>
2021-07-01 14:59:41 +0300

    Добавлен проект HomeWork-5 , считающий в раздельных потоках факториал числа N и сумму целых чисел до N.

---
+++ HomeWork-5/CalculationSumTask.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading;
+
+namespace HomeWork_5
+{
+    internal class CalculationSumTask
+    {
+        private readonly Thread _Thread;
+        private readonly int _Number;
+        private int _Result;
+
+        public int Result
+        {
+            get
+            {
+                Join();
+                return _Result;
+            }
+            private set => _Result = value;
+        }
+
+        public CalculationSumTask(int Number)
+        {
+            _Number = Number;
+            _Thread = new Thread(Calculation);
+        }
+
+        public void Start() => _Thread.Start();
+
+        private void Calculation()
+        {
+            var sum = 0;
+            for (var i = 1; i < _Number; i++)
+            {
+                sum += i;
+            }
+
+            _Result = sum;
+        }
+
+        public void Join() => _Thread.Join();
+    }
+}
---
+++ HomeWork-5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/*Кальник Виктор
+
+ 1. Написать приложение, считающее в раздельных потоках:
+a. факториал числа N, которое вводится с клавиатуру;
+b. сумму целых чисел до N, которое также вводится с клавиатуры.
+ */
+
+namespace HomeWork_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вас приветствует программа расчёта факториала и суммы целого числа.");
+            Console.Write("Введите целое число: ");
+            int number = GetInt();
+
+            var calculationFactorial = new CalculationFactorialTask(number);
+            calculationFactorial.Start();
+
+            var calculationSum = new CalculationSumTask(number);
+            calculationSum.Start();
+
+            Console.WriteLine($"\nФакториал числа {number} = {calculationFactorial.Result}");
+            Console.WriteLine($"Сумма чисел от 1 до {number} = {calculationSum.Result}");
+            
+            Console.WriteLine("\nРабота завершенна. Нажмите Enter для выхода.");
+            Console.ReadLine();
+        }
+        static int GetInt() //проверка ввода на число.
+        {
+            int x = 0;
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+                Console.Write("Неверный ввод, введите число: ");
+            }
+            return x;
+        }
+    }
+}
