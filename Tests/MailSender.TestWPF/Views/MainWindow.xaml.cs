bfda4c5dd5f6a190290031529491df8f41d82c05
Victor Kalnik <v.kalnik@mail.ru>
2021-07-05 03:28:42 +0300

    Использование диспетчера для взаимодействия с элементами UI из фонового потока

--- Tests/MailSender.TestWPF/Views/MainWindow.xaml.cs
+++ Tests/MailSender.TestWPF/Views/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 
 namespace MailSender.TestWPF
@@ -7,18 +8,39 @@ namespace MailSender.TestWPF
     {
         public MainWindow() => InitializeComponent();
 
-        //private void SendButton_OnClick(object sender, RoutedEventArgs e)
-        //{
 
-        //    using (var mailSenderServiceClass = new MailSenderServiceClass(LoginEdit.Text, PasswordEdit.SecurePassword, MailSenderTestWpfConfig.SmtpServer, MailSenderTestWpfConfig.SmtpServerPort, MailSenderTestWpfConfig.SendersAddress))
-        //    {
-        //        mailSenderServiceClass.Send(SubjectEdit.Text + " Время отправки: " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.ff"), BodyEdit.Text, MailSenderTestWpfConfig.RecipientAddress);
-        //    };
-        //}
+        private void Start_OnClick(object Sender, RoutedEventArgs E)
+        {
+            new Thread(() => Calculate()).Start();
+        }
 
-        //private void Exit_OnClick(object sender, RoutedEventArgs e)
-        //{
-        //    Close();
-        //}
+        private void Calculate(int Max = 100, int Timeout = 100)
+        {
+            var thread_id = Thread.CurrentThread.ManagedThreadId;
+
+            var dispatcher = Application.Current.Dispatcher;
+            
+            var s = 0;
+            for (var i = 0; i <= Max; i++)
+            {
+                //Application.Current.Dispatcher.BeginInvoke(new Action(
+                dispatcher.BeginInvoke(new Action(
+                    () =>
+                    {
+                        Progress.Value = i;
+                    }));
+
+                s += i;
+                Thread.Sleep(Timeout);
+            }
+
+            //Result.Dispatcher.Invoke(
+            dispatcher.Invoke(
+                () =>
+                {
+                    Result.Text = s.ToString();
+                });
+
+        }
     }
 }
