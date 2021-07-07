3aed53a8c854bd421ad6cbc4cc3d5d2b484e7879
Victor Kalnik <v.kalnik@mail.ru>
2021-07-07 05:56:06 +0300

    Выполено ДЗ к уроку 6: Приложение, производящее параллельное умножение 2 матриц 100х100. Матрицы заполняются случайными целыми числами от 0 до10.

---
+++ HomeWorke-6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HomeWorke_6
+{
+    class Program
+    {
+        private static int minValue = 0;
+        private static int maxValue = 11;
+        private static int arraySize = 100;
+        
+        static async Task Main(string[] args)
+        {
+            var array1 = MyArray.GenerateRandomArray(arraySize, arraySize, minValue, maxValue);
+            var array2 = MyArray.GenerateRandomArray(arraySize, arraySize, minValue, maxValue);
+            var arrayResult = await MyArray.ArrayMultiAsync(array1, array2);
+
+            //MyArray.PrintArray(array1);
+            //MyArray.PrintArray(array2);
+            //MyArray.PrintArray(arrayResult);
+
+            Console.WriteLine("Работа завершена, нажмите Enter для выхода.");
+            Console.ReadLine();
+        }
+    }
+}
