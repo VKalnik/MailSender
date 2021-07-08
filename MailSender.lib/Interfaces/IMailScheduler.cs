9b561bc325603ce861b6c5b0e51f6931a7ed7f50
Victor Kalnik <v.kalnik@mail.ru>
2021-07-08 19:28:12 +0300

    Интерфейс сервиса отправки почты

---
+++ MailSender.lib/Interfaces/IMailScheduler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MailSender.Models;
+
+namespace MailSender.Interfaces
+{
+    public interface IMailScheduler
+    {
+        void Start();
+        
+        void Stop();
+
+        SchedulerTask AddRoPlan(
+            DateTime Time,
+            Sender Sender,
+            EmailsList Recipients,
+            Server Server,
+            Message Message);
+    }
+}
