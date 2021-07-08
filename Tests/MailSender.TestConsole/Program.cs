6314b54b9f53bfcf81f594accb3fb3bf5f7e03e9
Victor Kalnik <v.kalnik@mail.ru>
2021-07-09 01:14:15 +0300

    Удаляем хлам

--- Tests/MailSender.TestConsole/Program.cs
+++ Tests/MailSender.TestConsole/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace MailSender.TestConsole
@@ -8,11 +7,6 @@ namespace MailSender.TestConsole
     {
         static async Task Main(string[] args)
         {
-            //TasksOverview.Run();
-            //TPL_Overview.Run();
-            //TasksOverview.RunAsync().Wait();
-            //await TasksOverview.RunAsync();
-            await ReadingFileTest.RunAsync();
 
 
             Console.WriteLine("Работа завершена, нажмите Enter для выхода.");
